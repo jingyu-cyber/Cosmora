dc364f6d42ec772e3dab535c97bd0dbcb1bc54f3
Jingyu <mhyjingyu@gmail.com>
2026-07-19 23:33:13 +0800

    bigscreen data

---
+++ bigscreen/src/data.js
@@ -0,0 +1,3 @@
+// Big screen shared data utilities
+export const provinces=["Beijing","Shanghai","Guangdong","Zhejiang","Jiangsu","Sichuan","Hubei","Shandong","Fujian","Hunan"];
+export const cityCoords={Beijing:[116.4,39.9],Shanghai:[121.5,31.2],Guangzhou:[113.3,23.1],Shenzhen:[114.1,22.5],Hangzhou:[120.2,30.3],Nanjing:[118.8,32.1],Chengdu:[104.1,30.6],Wuhan:[114.3,30.6],Jinan:[117.0,36.7]};
