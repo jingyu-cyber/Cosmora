0b81a42eacb4ae1842fa22904ae8f07d50006da5
Jingyu <mhyjingyu@gmail.com>
2026-07-19 23:14:10 +0800

    AppConfig

---
+++ backend/src/main/java/util/AppConfig.java
@@ -0,0 +1,1 @@
+package com.example.lessondesign.util; import org.springframework.context.annotation.*; import org.springframework.web.client.RestTemplate; @Configuration public class AppConfig { @Bean public RestTemplate restTemplate(){ return new RestTemplate(); } }
